f9719856499edbbcc504c15dd313a3d6ca5c908c
VardanMailyan <vardan.mailyan@tumo.org>
2023-12-02 19:11:52 +0400

    inhertiance

--- gameOfLife1/water.js
+++ gameOfLife1/water.js
@@ -1,51 +1,17 @@
-class Water {
+class Water extends LivingCreature{
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        super(x,y)
         this.energy = 15
         this.directions = []
     }
 
 
 
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
+   
 
     chooseCell(char, char1) {
         this.getNewCoordinates()
-        let found = []
-
-
-        for (let i in this.directions) {
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char) {
-                    found.push(this.directions[i])
-                }
-            }
-
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char1) {
-                    found.push(this.directions[i])
-                }
-            }
-        }
-
-
-
-
-        return found
+        return super.chooseCell(char , char1)
 
     }
 
